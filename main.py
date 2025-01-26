8285f41fe4d58e8a4306c9ca301fa257542924f3
leofusca <leotomasfusca@gmail.com>
2025-01-26 01:47:17 +0100

    CircleShape.py + Clock and fps

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_KINDS, ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
+    BLACK = (0,0,0)  
+   
+   
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(BLACK)
+        pygame.display.flip()
+
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
 if __name__ == "__main__":
     main() 
